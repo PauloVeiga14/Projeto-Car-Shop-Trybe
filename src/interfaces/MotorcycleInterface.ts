c9074d6911804e5c29758babf97171b56e5182b7
Paulo França <paulovinicius.franca@gmail.com>
2022-04-26 16:55:05 -0300

    Cria e implementa MotorcycleInterface

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import VehicleSchema from './VehicleInterface';
+
+const MotorcycleSchema = VehicleSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().int().positive().lte(2500),
+});
+
+type Motorcycle = z.infer<typeof MotorcycleSchema>;
+
+export default MotorcycleSchema;
+export { Motorcycle };
