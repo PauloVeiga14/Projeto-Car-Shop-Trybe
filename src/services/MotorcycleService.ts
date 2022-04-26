1308fab5ba607526463c362c6f834c8069270fa1
Paulo França <paulovinicius.franca@gmail.com>
2022-04-26 17:18:42 -0300

    Cria e implementa o MotorcycleService.ts

---
+++ src/services/MotorcycleService.ts
@@ -0,0 +1,38 @@
+import { ServiceError } from '../interfaces/ServiceError';
+import MotorcycleSchema,
+{ Motorcycle } from '../interfaces/MotorcycleInterface';
+import Service from './MongoService';
+import MotorcycleModel from '../models/MotorcycleModel';
+
+class MotorcycleService extends Service<Motorcycle> {
+  constructor(model = new MotorcycleModel()) {
+    super(model);
+  }
+
+  create = async (
+    obj: Motorcycle,
+  ): Promise<Motorcycle | ServiceError | null> => {
+    const parsed = MotorcycleSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+
+    return this.model.create(obj);
+  };
+
+  update = async (
+    id: string,
+    obj: Motorcycle,
+  ): Promise<Motorcycle | ServiceError | null> => {
+    const parsed = MotorcycleSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+
+    return this.model.update(id, obj);
+  };
+}
+
+export default MotorcycleService;
