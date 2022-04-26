c84cfd78a04a2fd18a3d7957e64c406341f03657
Paulo França <paulovinicius.franca@gmail.com>
2022-04-26 20:06:11 -0300

    Adiciona as informações mockadas para motorcycles

---
+++ src/tests/mocks/motorcycleMocks.ts
@@ -0,0 +1,47 @@
+export const data = {
+  newMotorcycle: {
+    model: "Honda CG Titan 125",
+    year: 1963,
+    color: "red",
+    buyValue: 3500,
+    category: "Street",
+    engineCapacity: 125
+  },
+  createdMotorcycle: {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Honda CG Titan 125",
+    year: 1963,
+    color: "red",
+    buyValue: 3500,
+    category: "Street",
+    engineCapacity: 125
+  },
+  arrayOfMotorcycles: [
+    {
+      _id: "4edd40c86762e0fb12000003",
+      model: "Honda CG Titan 125",
+      year: 1963,
+      color: "red",
+      buyValue: 3500,
+      category: "Street",
+      engineCapacity: 125
+    }
+  ],
+  updateMotorcycle: {
+    model: "Honda CG Titan 125",
+    year: 1964,
+    color: "red",
+    buyValue: 3500,
+    category: "Street",
+    engineCapacity: 125
+  },
+  updatedMotorcycle: {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Honda CG Titan 125",
+    year: 1964,
+    color: "red",
+    buyValue: 3500,
+    category: "Street",
+    engineCapacity: 125
+  },
+}
