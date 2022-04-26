c3dec7092c1a69d5199f254fe34c3f15bb3cc9da
Paulo França <paulovinicius.franca@gmail.com>
2022-04-26 09:20:58 -0300

    Cria o arquivo index e inicia o server

--- src/index.example.ts
+++ src/index.example.ts
@@ -1,3 +1,15 @@
-// import server from './server-example';
+// import CarController from './controllers/CarController';
+// import { Car } from './interfaces/CarInterface';
+// import CustomRouter from './routes/Router';
+// import App from './app';
+
+// const server = new App();
+
+// const carController = new CarController();
+
+// const carRouter = new CustomRouter<Car>();
+// carRouter.addRoute(carController);
+
+// server.addRouter(carRouter.router);
 
 // server.startServer();
