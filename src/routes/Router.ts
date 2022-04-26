5c33e5c8e2f91e67e8bab235b297d6de0beea481
Paulo França <paulovinicius.franca@gmail.com>
2022-04-26 08:11:12 -0300

    Cria e implementa o arquivo Router.ts

---
+++ src/routes/Router.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import Controller from '../controllers/MongoController';
+
+class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(controller: Controller<T>, route: string = controller.route) {
+    this.router.post(route, controller.create);
+    this.router.get(route, controller.read);
+    // this.router.get(`${route}/:id`, controller.readOne);
+    // this.router.patch(route, controller.update);
+    // this.router.post(route, controller.delete);
+  }
+}
+
+export default CustomRouter;
